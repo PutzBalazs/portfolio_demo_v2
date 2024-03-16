01603eb7e1e42f57d10b652ae4b5e18bb3c14769
PutzBalazs <124891846+PutzBalazs@users.noreply.github.com>
2024-03-16 15:07:50 +0100

    fix

--- crypto_index.py
+++ crypto_index.py
@@ -24,53 +24,7 @@ def get_price(symbol, interval, limit):
     return closed_prices
 
 
-def update(days, allocation):
-    # Test if weights are applied correctly
-    weight_sum = sum(allocation.values())
-    if weight_sum != 1:
-        print(f"Allocation weights do not sum to 1. Sum: {weight_sum}")
-
-    # Get historical prices for each cryptocurrency
-    historical_prices = {}
-    for symbol in allocation.keys():
-        prices = get_price(symbol, '1d', days)
-        historical_prices[symbol] = prices
-
-    # Adjust for weight in portfolio
-    for symbol, prices in historical_prices.items():
-        for i in range(0, days):
-            prices[i] *= allocation[symbol]
-
-    # Calculate the portfolio value for each day
-    portfolio_values = []
-    for i in range(days):
-        daily_portfolio_value = sum(historical_prices[symbol][i] for symbol in allocation)
-        portfolio_values.append(daily_portfolio_value)
-
-    df = pd.DataFrame(portfolio_values)
-
-    # Define the dates (last 7 days)
-    today = pd.Timestamp.now().floor('D')
-    dates = [today - pd.DateOffset(days=i) for i in range(days)]
-    # format dates
-    dates = [date.strftime('%Y-%m-%d') for date in dates]
-    df["Date"] = dates[::-1]
-    df.set_index("Date", inplace=True)
-
-    results_df = pd.DataFrame({
-        "Date": df.index,
-        "Portfolio_Value": portfolio_values,
-    })
-
-    # Save the results to a csv file
-    results_df.to_csv("portfolio_values.csv", index=False)
-
-
-# Adding a sidebar
-st.sidebar.title('Allocation')
-
-# Define initial allocation
-initial_allocation = {
+allocation = {
     "ETH": 0.14,
     "BNB": 0.09,
     "XRP": 0.13,
@@ -85,16 +39,59 @@ initial_allocation = {
     "AAVE": 0.02
 }
 
-# Input fields for allocation
-allocation = {}
-for crypto, weight in initial_allocation.items():
-    allocation[crypto] = st.sidebar.number_input(f"Allocation for {crypto}", min_value=0.0, max_value=1.0, value=weight, step=0.01)
 
-# Save the updated allocation to a csv file
-allocation_df = pd.DataFrame(allocation.items(), columns=["Crypto", "Allocation"])
-allocation_df.to_csv("allocation.csv", index=False)
+def update(days):
+    # Test if weights are applied correctly
+    weight_sum = sum(allocation.values())
+    if weight_sum != 1:
+        print(f"Allocation weights do not sum to 1. Sum: {weight_sum}")
+
+    #days = 700
+    # Get historical prices for each cryptocurrency
+    historical_prices = {}
+    for symbol in allocation.keys():
+        prices = get_price(symbol, '1d', days)
+        historical_prices[symbol] = prices
+    #print(f"Historiycal prices: {historical_prices}")
+    
+    # Adjust for weight in portfolio
+    for symbol, prices in historical_prices.items():
+        for i in range(0, days):
+            prices[i] *= allocation[symbol]
+
+    #print(f"Historiycal prices weighted: {historical_prices}")
+
+    # Calculate the portfolio value for each day
+    portfolio_values = []
+    for i in range(days):
+        daily_portfolio_value = sum(historical_prices[symbol][i] for symbol in allocation)
+        portfolio_values.append(daily_portfolio_value)
+
+    df = pd.DataFrame(portfolio_values)
+
+    # Define the dates (last 7 days)
+    today = pd.Timestamp.now().floor('D')
+    dates = [today - pd.DateOffset(days=i) for i in range(days)]
+    #format dates
+    dates = [date.strftime('%Y-%m-%d') for date in dates]
+    df["Date"] = dates[::-1]
+    df.set_index("Date", inplace=True)
+
+    results_df = pd.DataFrame({
+        "Date": df.index,
+        "Portfolio_Value": portfolio_values,
+    })
+    
+    # Save the results to a csv file
+    results_df.to_csv("portfolio_values.csv", index=False)
+
+
+# Adding a sidebar
+st.sidebar.title('Allocation')
 
 # Pie chart for allocations
+allocation_df = pd.DataFrame(allocation.items(), columns=["Crypto", "Allocation"])
+
 st.markdown("## Allocations")
 fig, ax = plt.subplots(figsize=(4, 4))
 fig.patch.set_facecolor((38 / 255, 39 / 255, 48 / 255))  # Set background color
@@ -107,14 +104,17 @@ ax.legend(wedges, alloc_txt,
           bbox_to_anchor=(1, 0, 0.5, 1),
           fontsize='small')
 
-# Plotting initial Line Chart
+# Plotting
+st.sidebar.pyplot(fig)
+
+# Display Line Chart
 results_df = pd.read_csv("portfolio_values.csv")
-line_chart = st.line_chart(results_df.set_index("Date"), use_container_width=True)
+st.line_chart(results_df.set_index("Date"), use_container_width=True)
 
 # Input field for number of days
 days_input = st.sidebar.number_input("Number of Days (max 500)", min_value=1, max_value=500, value=100)
 
 # Button to update the chart
 if st.sidebar.button("Update Chart"):
-    update(days_input, allocation)
+    update(days_input)
     st.rerun()
